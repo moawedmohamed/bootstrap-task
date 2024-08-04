dcd84b810c6d038d7709011ef6661c9402b1fdef
moawedmohamed <moawedmohamed144@gmail.com>
2024-08-05 01:16:13 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// let paper = prompt('Enter the number of papers');
+
+// if (isNaN(paper)) {
+//     alert("You must enter a number");
+// } else {
+//     paper = Number(paper);
+//     if (paper > 20) {
+//         console.log('You are allowed to attend the exam');
+//     } else {
+//         console.log('You are not allowed to attend the exam');
+//     }
+// }
